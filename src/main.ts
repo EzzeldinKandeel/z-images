1c7180fc0a9cdee9d815716bc5d3fc083dfd6bac
Ezzeldin Kandeel <ezzeldinkandeel@gmail.com>
2025-11-27 13:30:26 +0200

    Added helmet

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { EnvironmentVariables } from './env.validation';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   // (ConfigService) is injected differently outside of modules:
   const configService = app.get(ConfigService<EnvironmentVariables>);
   // The built-in (ValidationPipe) uses the 'class-validator' package

bab488a7ed8733a9832b8194ae05e58f16e250b7
Ezzeldin Kandeel <ezzeldinkandeel@gmail.com>
2025-12-16 18:58:20 +0200

    Added Swagger (not completely configured)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { EnvironmentVariables } from './env.validation';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+
   // (ConfigService) is injected differently outside of modules:
   const configService = app.get(ConfigService<EnvironmentVariables>);
+
+  const config = new DocumentBuilder()
+    .setTitle('z-images')
+    .setDescription('The image storage and maniputlation API')
+    .setVersion('1.0')
+    .addTag('images')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   // The built-in (ValidationPipe) uses the 'class-validator' package
   app.useGlobalPipes(
     new ValidationPipe({
